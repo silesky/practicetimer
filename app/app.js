13624f7b9278d11004d811adaabf00855ea38852
sethicus <seth@sethsilesky.com>
2016-02-23 21:17:08 +0000

    added my first subcomponent: BtnCloseTimer

--- app/app.js
+++ app/app.js
@@ -2,18 +2,37 @@ import React from "react";
 // import Greeting from "./greeting";
 console.log("app.js...")
 
+var BtnAddTimer = React.createClass({
+  render: function() {
+    return(
+      <div className="btnAddTimer">ADD</div>
+      );
+    
+  }
+});
+
+//BtnCloseTimer, BtnCopyTimer, BtnAddTimer, CountDown, StartTimer
 
 var TimerBox = React.createClass({
   render: function() {
     return(
-      <div className="timerBox">I'm a timer</div>
+      <div className="timerBox">
+        <BtnAddTimer />
+      </div>
       );
   }     
 });
 
+
+
+
+
+
+/* drum roll */
 React.render(
-  //<Greeting name="World"/>
+
   <TimerBox />,
   
   document.getElementById('timer')//where u want it
 );
+
