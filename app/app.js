4e661b2694710de4065f579afd53f7dfb9d797a9
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-03-01 10:47:00 -0600

    pause, play, and reset working

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 /* jshint
 esversion: 6  */
-/* http://localhost:9080/app */
+/* TODO
+. add an edit button (when you click the number, it should change to a form)
+. find a way to pause the timer... create a "ticking 'state'"
+*/
+/* webpack --progress --colors --watch */
 import React from 'react';
 import ReactDOM from 'react-dom';
 console.log("app.js...");
@@ -37,27 +41,41 @@ var Title = React.createClass({
 
 /*** CountDown ***/
 var CountDown = React.createClass({
-
+  tick: function() {
+    // every time this is called, counter goes down by 1
+    this.setState({secondsElapsed: this.state.secondsElapsed - 1});
+  },
   countDownStart: function() {
+        this.setState({ticking: true});
         this.interval = setInterval(this.tick, 1000);
+
+  },
+  countDownStop: function() {
+        clearInterval(this.interval);
   },
   reset: function() {
-
     var stateObj = this.getInitialState();
     var newNum = stateObj.secondsElapsed;
-
     this.setState({
         secondsElapsed: newNum
       });
   },
+
+
   edit: function() {
-    var newNum =  this.refs.editNum.getDOMNode().value;
+    var v =  this.refs.editNum.getDOMNode().value;
     this.setState({
-        secondsElapsed: newNum
+        secondsElapsed: v
       });
   },
   pausePlay: function() {
-    console.log("pause-play");
+     if (!this.state.ticking) {
+      console.log("pause-play");
+      this.countDownStart();
+    } else {
+      this.state.ticking = false;
+      this.countDownStop();
+    }
   },
   incrementTime: function() {
     console.log("increment");
@@ -66,38 +84,44 @@ var CountDown = React.createClass({
     console.log("decrement");
   },
   getInitialState: function() {
-    return {secondsElapsed: 2700};
-  },
-  tick: function() {
-    // every time this is called, counter goes down by 1
-    this.setState({secondsElapsed: this.state.secondsElapsed - 1});
+    return {secondsElapsed: 2700,
+                  ticking: false};
   },
   componentDidMount: function() {
     // (???) called right after render. setInterval takes calls this.tick every 10000ms...
-    this.countDownStart();
   },
   componentWillUnmount: function() {
     // (???) called at the end, right before the component is destroyed/deleted...
-    clearInterval(this.interval);
+    this.countDownStop();
   },
   render: function() {
-    return(
+    var timerText;
+    if (this.state.secondsElapsed > 0) {
+      timerText = this.state.secondsElapsed;
+    } else {
+      timerText = "done.";
+      this.countDownStop();
+    }
+    return (
+
       <div className="countDownContainer">
 
         <input type="number" placeholder="new time" ref="editNum" />
-        <input type="submit" onClick={this.edit}/>
-
-        <div>{this.state.secondsElapsed}</div>
-          <div className="setTimerUpDnContainer">
-            <div className="setTimerUpComp"
-              onClick={this.incrementTime}>[up]</div>
-            <div className="setTimerDnComp"
-              onClick={this.decrementTime}>[dn]</div>
-          </div>
-          <div className="countDownBtnReset"
-            onClick={this.reset}>[r]</div>
-          <div className="countDownBtnPausePlay"
+        <button label="stuff" type="button" onClick={this.edit}>OK</button>
+        <div className="countDownBtnPausePlay btn"
             onClick={this.pausePlay}>[=>]</div>
+
+        <div>{timerText}</div>
+
+        <div className="setTimerUpDnContainer btn">
+          <div className="setTimerUpComp"
+            onClick={this.incrementTime}>[up]</div>
+          <div className="setTimerDnComp"
+            onClick={this.decrementTime}>[dn]</div>
+        </div>
+         <div className="countDownBtnReset btn"
+          onClick={this.reset}>[r]</div>
+
 
       </div>
 
