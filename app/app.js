c1d925762e81dbbeb1b247b7f2a97e4d04e02142
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-03-03 19:35:12 -0600

    got css color changing working:

--- app/app.js
+++ app/app.js
@@ -19,25 +19,30 @@ var Title = React.createClass({
   getInitialState: function() {
       return {
         titletext: "default state",
+        style: {background: 'white' }
       };
   },
 
   handleTitleInput: function(e) {
     console.log("handleTitleInput: " + e.target.value);
-    this.setState({titletext: e.target.value});
-
+    this.setState({
+      titletext: e.target.value,
+      style: {background: 'white'}
+    });
   },
 
   handleTitleBlur: function() {
-    this.style = {
-      background: 'red'
-    };
+    this.setState({
+      style: {background: 'red',
+                  color: 'white'}
+    });
+
   },
 
   render: function() {
     return (
       <div>
-      <input type="text" onBlur={this.handleTitleBlur} onChange={this.handleTitleInput}/>
+      <input style={this.state.style} type="text" onBlur={this.handleTitleBlur} onFocus={this.handleTitleInput}/>
       <div>{this.state.titletext}</div>
       </div>
     );
