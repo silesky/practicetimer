1abc3ad45dcafea0acb24ee352d91c8587800c47
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-03-08 11:23:49 -0600

    edited bash file

--- misc-2.sh
+++ misc-2.sh
@@ -1,5 +1,19 @@
 # echo "... W E B P A C K ..."
 # webpack --progress --colors --watch
+# if (webpack is running)  {
+ # $running = true/false
+#...
+
+# if ($running = true) {
+  #echo "do you wish to START webpack?"
+  # $answer = true/false
+# } select yn ...
+
+
+# if ($running = false) {
+  #echo "do you wish to STOP webpack?"
+  # $answer = true/false
+# } select yn ...
 
 echo "Do you wish to start webpack?"
 select yn in "Yes" "No"; do
