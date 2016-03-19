f4e5a24afc921a265572785138f4980b46255f50
silesky <seth.silesky@gmail.com>
2016-03-19 12:21:05 -0500

    break CountDown into separate component

---
+++ app/CountDown.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+  /* C o u n t D o w n: timerbox and boards kid */
+var CountDown = React.createClass({
+
+  getInitialState: function () {
+    return { secondsElapsed: 10,
+        ticking: false,
+        paused: true };
+  },
+  componentDidMount: function () {
+        // (???) called right after render. setInterval takes calls this.tick every 10000ms...
+  },
+  componentWillUnmount: function () {
+        // (???) called at the end, right before the component is destroyed/deleted...
+    this.countDownStop();
+  },
+  tick: function () {
+        // every time this is called, counter goes down by 1
+    this.setState({ secondsElapsed: this.state.secondsElapsed - 1 });
+  },
+  countDownStart: function () {
+    clearInterval(this.interval);
+    this.interval = setInterval(this.tick, 1000);
+    this.setState({ ticking: true });
+  },
+  countDownStop: function () {
+    clearInterval(this.interval);
+    this.setState({ ticking: false });
+  },
+  reset: function () {
+    this.countDownStop();
+    var stateObj = this.getInitialState();
+    var newNum = stateObj.secondsElapsed;
+    this.setState({
+        secondsElapsed: newNum
+      });
+  },
+  handleNumInput: function (e) {
+    console.log(e.target.value);
+    this.setState({
+        secondsElapsed: e.target.value
+      });
+  },
+  pausePlay: function () {
+    if (!this.state.ticking) {
+        console.log('pause-play');
+        this.countDownStart();
+        this.setState({ paused: false });
+      } else {
+        this.setState({ paused: true });
+        this.countDownStop();
+      }
+  },
+  incrementTime: function () {
+    this.setState({
+        secondsElapsed: this.state.secondsElapsed + 1
+      });
+
+  },
+  decrementTime: function () {
+    this.setState({
+        secondsElapsed: this.state.secondsElapsed - 1
+      });
+  },
+ 
+  render: function () {
+
+    var timerText;
+    if (this.state.secondsElapsed > 0) {
+        timerText = this.state.secondsElapsed;
+      } else {
+        timerText = 'done.';
+        clearInterval(this.interval);
+      }
+    return (
+          <div>
+            <div className="countDownSettingsContainer">
+              <input
+                type="number"
+                placeholder="new time"
+                onChange={this.handleNumInput} />
+              <button
+                label="stuff"
+                type="button"
+                onClick={this.edit}>OK</button>
+              <div
+                className="countDownBtnPausePlay btn"
+                onClick={this.pausePlay}>[=>]</div>
+              <div className="setTimerUpDnContainer btn">
+                <div
+                  className="setTimerUpComp"
+                  onClick={this.incrementTime}>[up]</div>
+                <div
+                  className="setTimerDnComp"
+                  onClick={this.decrementTime}>[dn]</div>
+              </div>
+              <div
+                className="countDownBtnReset btn"
+                onClick={this.reset}>[r]</div>
+
+            </div>
+            <div className="countDownTextContainer">
+              <div className="countDownText">{timerText}</div>
+            </div>
+          </div>
+
+
+        );
+  }
+});
+module.exports = CountDown;
--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 
-/*
-webpack --progress --colors --watch
-*/
+
+//webpack --progress --colors --watch
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-// var CountDownTotal = require('./CountDownTotal');
 import CountDownTotal from './CountDownTotal';
+import CountDown from './CountDown';
+
 import $ from 'jquery';
 import draggable from 'jquery-ui';
 
@@ -52,116 +53,6 @@ var Title = React.createClass({
   }
 
 });
-  /* C o u n t D o w n: timerbox and boards kid */
-var CountDown = React.createClass({
-
-  getInitialState: function () {
-    return { secondsElapsed: 10,
-        ticking: false,
-        paused: true };
-  },
-  componentDidMount: function () {
-        // (???) called right after render. setInterval takes calls this.tick every 10000ms...
-  },
-  componentWillUnmount: function () {
-        // (???) called at the end, right before the component is destroyed/deleted...
-    this.countDownStop();
-  },
-  tick: function () {
-        // every time this is called, counter goes down by 1
-    this.setState({ secondsElapsed: this.state.secondsElapsed - 1 });
-  },
-  countDownStart: function () {
-    clearInterval(this.interval);
-    this.interval = setInterval(this.tick, 1000);
-    this.setState({ ticking: true });
-  },
-  countDownStop: function () {
-    clearInterval(this.interval);
-    this.setState({ ticking: false });
-  },
-  reset: function () {
-    this.countDownStop();
-    var stateObj = this.getInitialState();
-    var newNum = stateObj.secondsElapsed;
-    this.setState({
-        secondsElapsed: newNum
-      });
-  },
-  handleNumInput: function (e) {
-    console.log(e.target.value);
-    this.setState({
-        secondsElapsed: e.target.value
-      });
-  },
-  pausePlay: function () {
-    if (!this.state.ticking) {
-        console.log('pause-play');
-        this.countDownStart();
-        this.setState({ paused: false });
-      } else {
-        this.setState({ paused: true });
-        this.countDownStop();
-      }
-  },
-  incrementTime: function () {
-    this.setState({
-        secondsElapsed: this.state.secondsElapsed + 1
-      });
-
-  },
-  decrementTime: function () {
-    this.setState({
-        secondsElapsed: this.state.secondsElapsed - 1
-      });
-  },
- 
-  render: function () {
-
-    var timerText;
-    if (this.state.secondsElapsed > 0) {
-        timerText = this.state.secondsElapsed;
-      } else {
-        timerText = 'done.';
-        clearInterval(this.interval);
-      }
-    return (
-          <div>
-            <div className="countDownSettingsContainer">
-              <input
-                type="number"
-                placeholder="new time"
-                onChange={this.handleNumInput} />
-              <button
-                label="stuff"
-                type="button"
-                onClick={this.edit}>OK</button>
-              <div
-                className="countDownBtnPausePlay btn"
-                onClick={this.pausePlay}>[=>]</div>
-              <div className="setTimerUpDnContainer btn">
-                <div
-                  className="setTimerUpComp"
-                  onClick={this.incrementTime}>[up]</div>
-                <div
-                  className="setTimerDnComp"
-                  onClick={this.decrementTime}>[dn]</div>
-              </div>
-              <div
-                className="countDownBtnReset btn"
-                onClick={this.reset}>[r]</div>
-
-            </div>
-            <div className="countDownTextContainer">
-              <div className="countDownText">{timerText}</div>
-            </div>
-          </div>
-
-
-        );
-  }
-});
-
 
 
     /* T i m e r B o x: boards kid */
