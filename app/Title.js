d669db9cec44e97c32199e8b3fdff72f7a7fde71
Seth Silesky <ssilesky@Seths-Tulane-Macbook.local>
2016-03-22 18:53:54 -0500

    redux logic to title component

--- app/Title.js
+++ app/Title.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from '../node_modules/redux';
+import { createStore } from 'redux';
+
+function reduxTest() {
+  
+  const reducer = (state = 0, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return state + 1;
+      default:
+        return state;
+    }
+  }
+  const store = createStore(reducer);
+
+  console.log(store.getState());
+  var node = document.getElementById('timer');
+  node.addEventListener('click', function () {
+    store.dispatch({ type: 'INCREMENT' });
+    console.log(store.getState());
+  });
+
+}
+reduxTest();
 
 var Title = React.createClass({
   getInitialState: function () {
