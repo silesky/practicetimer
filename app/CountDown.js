d465cf3b1c518c49b51798c88d624806483108bb
silesky <seth.silesky@gmail.com>
2016-03-26 16:14:24 -0500

    change to switch single quote

--- app/CountDown.js
+++ app/CountDown.js
@@ -11,15 +11,15 @@ function reducer(state, action) {
         switch (action.type) {
           case 'INCREMENT':
             console.log('INCREMENT');
-            console.log("state:" + (state + 1));
+            console.log('state:' + (state + 1));
             return state + 1;
           case 'PAUSEPLAY':
             console.log('PAUSEPLAY');
-            console.log("state:" + 0);
+            console.log('state:' + 0);
             return 0;
           case 'DECREMENT':
             console.log('DECREMENT');
-            console.log("state:" + (state - 1));
+            console.log('state:' + (state - 1));
             return state - 1;
           default:
             return state
