9cb9188ca52b6fa27fd980dd471639119ed12545
Seth Silesky <seth.silesky@gmail.com>
2016-03-31 15:42:03 -0500

    removed extraneous connect

--- app/CountDown.js
+++ app/CountDown.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { connect } from 'react-redux';
 import { createStore } from 'redux';
 
 /* put this in reducer.js eventually */
