1637ebe6563fb545780c97c09f1f8a90e45fa246
sethsilesky <seth.silesky@gmail.com>
2016-04-10 18:07:09 -0500

    add actions INCREMENT_TIMERCOUNT, DECREMENT_TIMERCOUNT

--- app/reducers/_Reducer.js
+++ app/reducers/_Reducer.js
@@ -3,44 +3,47 @@ const reducer = function reducer(state = {
       id: 0,
       time: 0,
       text: 'enter text',
+      timerCount: 1,
     },
     action) {
-                                                console.log('reducer called');
-                                                console.log(state);
 
+    console.log('reducer called');
+    console.log(state);
 
     switch (action.type) {
-
+      case 'INCREMENT_TIMERCOUNT':
+        return {
+          timerCount: state.timerCount + 1,
+        };
+        case 'DECREMENT_TIMERCOUNT':
+          return {
+            timerCount: state.timerCount - 1,
+          };
       case 'SET_TITLE':
-          return {
-            id: action.id,
-            time: state.time,
-            text: action.text,
-          };
+        return {
+          id: action.id,
+          time: state.time,
+          text: action.text,
+        };
       case 'RESET':
-                                                    console.log('RESET');
-            return {
-              time: 0, /* should simply return
+        return {
+          time: 0, /* should simply return
               the current time, and then clearInterval (stop ticking). */
-            };
+        };
       case 'INCREMENT':
-                                                    console.log('INCREMENT');
-          return {
-            time: state.time + 1,
-          };
-        case 'DECREMENT':
-                                                      console.log('DECREMENT');
-            return {
+        return {
+          time: state.time + 1,
+        };
+      case 'DECREMENT':
+        return {
               time: state.time - 1,
-            };
-        case 'PAUSEPLAY':
-                                                      console.log('PAUSEPLAY');
-          return state;
+        };
+      case 'PAUSEPLAY':
+        return state;
           /* should simply return
           the current time, and then clearInterval (stop ticking). */
 
-        case 'SET_TIME':
-                                                    console.log('SET THE TIME');
+      case 'SET_TIME':
         return {
             time: Number(action.time),
           };
