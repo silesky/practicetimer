1d4bb8a5e22adf13d55544f2fe053c223b6fe366
sethsilesky <seth.silesky@gmail.com>
2016-04-22 13:49:00 -0500

    Title: title component form renders
    https://trello.com/c/mNqC7MvL

--- app/reducers/timerBoxReducer.js
+++ app/reducers/timerBoxReducer.js
@@ -3,116 +3,38 @@ import boardReducer from './boardReducer';
 const timerBoxReducer = function(
   state = [{ id: 1, time: 10, title: 'shop' }, { id: 2, time: 5, title: 'work' }], action) {
 
-
+    // copy the state array
+    let stateCopyArr = state.slice(0);
+    // search in the array, return the object w/ the id that belongs to the clicked element
+    let foundObj = stateCopyArr.find((el) => el.id === action.id);
+    // grab the index of the object to remove
+    const foundIndex = stateCopyArr.indexOf(foundObj);
+    // copy the array and remove the found element, and replace it with the new element
     switch (action.type) {
-      case 'ADD_TIMER':
-            console.log('add!');
-      return [...state, {id: 3, time: 10, title: 'shop' }];
-
-      case 'INCREMENT':
-      // copy the state array
-      // const stateCopyArr = state.slice(0);
-      // search in the array, return the object with the right id
-      // const foundObj = stateCopyArr.find((el) => el.id === action.id);
-      console.log(foundObj);
-      // change that object
-      // foundObj.time =  foundObj.time + 1;
-      // remove the old object and replace it
-      // get the index of the old object
-      // var indexOfElToRemove = stateCopyArr.indexOf(foundObj);
-      // use array.splice to replace it with the new object
-      //   stateCopyArr.splice(indexOfElToRemove, 1, foundObj);
-      //  console.log(stateCopyArr.time);
-      // return stateCopyArr;
-
-      case 'DECREMENT':
-      console.log('DECREMENT');
-
-      case 'REMOVE_TIMER':
-      // copy the state array
-      const stateCopyArr = state.slice(0);
-      // search in the array, return the object w/ the id that belongs to the clicked element
-      const foundObj = stateCopyArr.find((el) => el.id === action.id);
-      // grab the index of the object to remove
-      const foundIndex = stateCopyArr.indexOf(foundObj);
-      // now that you know where it is in the array, remove the object
-      stateCopyArr.splice(foundIndex, 1);
+      case 'SET_TITLE':
+      // grab the node
+        const newText = action.text;
+     // remove the old node
+        console.log(foundObj.title);
+      // create the new object e.g {id: 2, time: 3, title: newText }
+      stateCopyArr[foundIndex].title = newText;
       return stateCopyArr;
 
+      case 'ADD_TIMER':
+            console.log('add!');
+      return [...state, { id: 3, time: 10, title: 'shop' }];
+      case 'INCREMENT':
+        console.log('INCREMENT');
+      case 'DECREMENT':
+        console.log('DECREMENT');
+      case 'REMOVE_TIMER':
 
+        // now that you know where it is in the array, remove the old object
+        stateCopyArr.splice(foundIndex, 1);
+        return stateCopyArr;
       default:
       return state;
     }
   };
-
-
-  //
-  // const timerBoxReducer = function(
-  //   state =
-  //    { timeArray: [100],
-  //      idArray: [1],
-  //      titleArray: [''],
-  //    },
-  //    action) {
-  //       const lastArrayValue = (arr) => arr[arr.length - 1];
-  //       function removeItemFromArray(array, itemToRemove) {
-  //         var indexToRemove = array.indexOf(itemToRemove);
-  //         const copyArray = array.slice();
-  //         copyArray.splice(indexToRemove, 1);
-  //         return copyArray;
-  //       }
-  //       const nextId = lastArrayValue(state.idArray);
-  //
-  //     switch (action.type) {
-  //       case 'REMOVE_TIMER':
-  //       // add another array item to the end of timeArray, incremented by one.
-  //
-          //  const lastArrayValue = (arr) => arr[arr.length - 1];
-          //   const nextId = lastArrayValue(state.idArray);
-  //       const newIdArray = removeItemFromArray(state.idArray, action.id);
-  //           console.log(newIdArray);
-  //         console.log(action);
-  //             return Object.assign({}, state, {
-  //               timerCount: state.timerCount - 1,
-  //               timeArray: [...state.timeArray, (lastArrayValue(state.timeArray) - 1)],
-  //               idArray: newIdArray
-  //             });
-  //
-  //       case 'SET_TITLE':
-  //       const oldArrayCopy = state.titleArray.slice();
-  //       oldArrayCopy.splice(action.id, 1, action.text);
-  //
-  //       return Object.assign({}, state, {
-  //         titleArray: oldArrayCopy
-  //       });
-  //       case 'RESET':
-  //       return Object.assign({}, state, {
-  //         time: 0,
-  //       });
-  //       case 'INCREMENT':
-  //       return Object.assign({}, state, {
-  //         time: state.time + 1,
-  //       });
-  //       case 'DECREMENT':
-  //       return Object.assign({}, state, {
-  //         time: state.time - 1,
-  //       });
-  //       case 'PAUSEPLAY':
-  //       return state;
-  //
-  //       /* should simply return
-  //       the current time, and then clearInterval (stop ticking). */
-  //
-  //       case 'SET_TIME':
-  //       return Object.assign({}, state, {
-  //         time: Number(action.time),
-  //       });
-  //       default:
-  //       return state;
-  //     }
-  //
-  //   };
-  //
-  //
 
   module.exports = timerBoxReducer;
