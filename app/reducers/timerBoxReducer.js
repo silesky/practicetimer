455ef4acec0a85ab1b4d02c5779dc2ea3e79fae1
sethsilesky <seth.silesky@gmail.com>
2016-04-25 17:05:51 -0500

    random things happening on button click fix: all reducers returning state

--- app/reducers/timerBoxReducer.js
+++ app/reducers/timerBoxReducer.js
@@ -11,13 +11,16 @@ const timerBoxReducer = function(
 
     const util = {
       getState_replaceElByIndex: (index, el) => {
+        console.log('getState_replace...');
         return [
           ...state.slice(0, index),
           state[index] =  el,
           ...state.slice(index + 1)
         ];
+
       },
       getState_removeElByIndex: (index) => {
+                console.log('getState_remove...');
         return [
           ...state.slice(0, index),
           ...state.slice(index + 1)
@@ -27,24 +30,29 @@ const timerBoxReducer = function(
         return Math.max(...arr.map(el => el['id'])) + 1;
       },
       getCurrentIndex: () => {
+
         const foundObj = state.find((el) => el.id === action.id);
         const foundIndex = state.indexOf(foundObj);
         return foundIndex;
       },
       getCurrentObj: () => {
+        console.log('getCurrentObj...');
          return state.find((el) => el.id === action.id);
        },
     };
     switch (action.type) {
       case 'SET_TITLE':
+      return state;
       case 'ADD_TIMER':
-      return [...state, { id: util.getNextId, time: 10, title: 'shop' }];
+      return [...state, { id: util.getNextId, time: 10, title: 'new timer' }];
 
       case 'INCREMENT':
       console.log('INCREMENT');
+      return state;
 
       case 'DECREMENT':
-      console.log('DECREMENT');
+      console.log('INCREMENT');
+      return state;
 
       case 'REMOVE_TIMER':
       return util.getState_removeElByIndex(util.getCurrentIndex());
