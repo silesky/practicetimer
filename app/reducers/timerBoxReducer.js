4f9326618069a446ef953c58fee8f9452e5b95f8
sethsilesky <seth.silesky@gmail.com>
2016-04-25 11:56:11 -0500

    auto indent

--- app/reducers/timerBoxReducer.js
+++ app/reducers/timerBoxReducer.js
@@ -13,21 +13,21 @@ const timerBoxReducer = function(
 
     const util = {
       replaceElByIndex: (array, index, el) => {
-          return [
+        return [
           ...array.slice(0, index),
           array[index] =  el,
           ...array.slice(index + 1)
-          ];
-          },
-        removeElByIndex: (array, index) => {
-          return [
+        ];
+      },
+      removeElByIndex: (array, index) => {
+        return [
           ...array.slice(0, index),
           ...array.slice(index + 1)
-          ];
-        },
-        getNextId: (arr) => {
-          return Math.max(...arr.map(el => el['id'])) + 1;
-        }
+        ];
+      },
+      getNextId: (arr) => {
+        return Math.max(...arr.map(el => el['id'])) + 1;
+      }
     };
 
     // copy the array and remove the found element, and replace it with the new element
@@ -35,25 +35,28 @@ const timerBoxReducer = function(
       case 'SET_TITLE':
       // grab the node
       console.log(state);
-        const newText = action.text;
+      const newText = action.text;
       stateCopyArr[foundIndex].title = newText;
       return stateCopyArr;
 
       case 'ADD_TIMER':
-            console.log('add!');
+      console.log('add!');
       return [...stateCopyArr, { id: util.getNextId, time: 10, title: 'shop' }];
+
       case 'INCREMENT':
-        console.log('INCREMENT');
+      console.log('INCREMENT');
+
       case 'DECREMENT':
-        console.log('DECREMENT');
+      console.log('DECREMENT');
+
       case 'REMOVE_TIMER':
 
       return util.removeElByIndex(state, foundIndex);
-        // now that you know where it is in the array, remove the old object
-        // stateCopyArr.splice(foundIndex, 1);
+      // now that you know where it is in the array, remove the old object
+      // stateCopyArr.splice(foundIndex, 1);
 
       default:
-        return state;
+      return state;
     }
   };
 
