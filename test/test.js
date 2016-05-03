16f5e2175b776bbc71dc2cb809220cc5b93e890a
Seth Silesky <seth.silesky@gmail.com>
2016-05-03 14:58:29 -0500

    write mocha tests for ticking reducer

--- test/test.js
+++ test/test.js
@@ -1,14 +1,32 @@
-//mocha test
+import { assert } from 'Chai';
+import React from 'react';
+import redux from 'redux';
+import store from '../app/_store.js';
+import reducer from '../app/reducers/_Reducer.js';
+// import * as support from 'support';
+// import API from 'api-vbb'
 
-
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
+describe('MY MOCHA TEST', function() {
+  describe('indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
 });
-console.log("nice");
-var app = require('../app/app');
+
+describe('REDUCER->TICKING TEST GROUP:', function() {
+  describe('store.dispatch: SET_TICKING_TRUE', function() {
+    it('store.getState()[0].ticking should return true when action: SET_TICKING_TRUE is dispatched...', function() {
+      store.dispatch({ type: 'SET_TICKING_TRUE', id: 1 })
+      assert.equal(store.getState()[0].ticking, true);
+    });
+  });
+  describe('store.dispatch: SET_TICKING_FALSE', function() {
+    it('store.getState()[0].ticking should return false after action: SET_TICKING_FALSE is dispatched...', function() {
+      store.dispatch({ type: 'SET_TICKING_FALSE', id: 1 });
+      assert.equal(store.getState()[0].ticking, false);
+    });
+  });
+
+});
