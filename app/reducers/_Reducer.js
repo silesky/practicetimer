e0781b196fa63d64ffc0439982d1df0ac95150f0
sethsilesky <seth.silesky@gmail.com>
2016-05-09 22:10:50 -0500

    indent reducer

--- app/reducers/_Reducer.js
+++ app/reducers/_Reducer.js
@@ -29,39 +29,40 @@ const reducer = function(state = initialState, action) {
         return Math.max(...state.map(el => el['id'])) + 1;
       },
     };
-    var _individualTimerObjEl = util.getCurrentObjEl();
-    var _currentIndex = util.getCurrentIndex();
-console.log(state);
+    // using let as a signal that the individualTimerObj's properties is going to be mutated, even if it itself will never be reassigned.
+    let _individualTimerObjEl = util.getCurrentObjEl();
+    let _currentIndex = util.getCurrentIndex();
+
     switch (action.type) {
       case 'SET_TICKING_TRUE':
       console.log('SET_TICKING_TRUE');
-      if (_individualTimerObjEl !== 'undefined')   {
-        _individualTimerObjEl.ticking =  true;
-      }
+        if (_individualTimerObjEl !== 'undefined')   {
+          _individualTimerObjEl.ticking =  true;
+        }
       return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
       case 'SET_TICKING_FALSE':
-      console.log('SET_TICKING_FALSE');
+        console.log('SET_TICKING_FALSE');
         if (_individualTimerObjEl !== 'undefined') {
           _individualTimerObjEl.ticking = false;
       }
-      return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
+        return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
       case 'SET_TITLE':
-      _individualTimerObjEl.title = action.text;
-      return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
-      // at the moment that add_timer is instantiated, the state only has two timers
+        _individualTimerObjEl.title = action.text;
+        return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
+        // at the moment that add_timer is instantiated, the state only has two timers
       case 'ADD_TIMER':
-      return [...state, { id: util.getNextId(), time: 10, title: 'new timer', ticking: false }];
+        return [...state, { id: util.getNextId(), time: 10, title: 'new timer', ticking: false }];
       case 'INCREMENT':
-      console.log('INCREMENT');
-      _individualTimerObjEl.time = _individualTimerObjEl.time + 1;
-      return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
+        console.log('INCREMENT');
+        _individualTimerObjEl.time = _individualTimerObjEl.time + 1;
+        return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
       case 'DECREMENT':
-      _individualTimerObjEl.time = _individualTimerObjEl.time - 1;
-      return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
+        _individualTimerObjEl.time = _individualTimerObjEl.time - 1;
+        return util.getState_replaceElByIndex(_currentIndex, _individualTimerObjEl);
       case 'REMOVE_TIMER':
-      return util.getState_removeElByIndex(_currentIndex, _individualTimerObjEl);
+        return util.getState_removeElByIndex(_currentIndex, _individualTimerObjEl);
       default:
-      return state;
+        return state;
     }
   };
 
