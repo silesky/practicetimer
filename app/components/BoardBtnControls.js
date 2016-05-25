d6c983a5f350bab8d86beabd22942eb70a57ef2a
Seth Silesky <seth.silesky@gmail.com>
2016-05-25 14:49:45 -0500

    move play button to board component, add fontawesome

--- app/components/BoardBtnControls.js
+++ app/components/BoardBtnControls.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const BoardBtnControls = React.createClass({
-  render: function() {
+const BoardBtnControls = ({ onBoardBtnPausePlayClick}) => {
     return (
-      <div className="BoardBtnControls">
-        <div className="BoardBtnPausePlay">
-          =>
+
+    <div className="BoardBtnControls">
+
+        <div className="BoardBtnPausePlay"
+              onClick={() => {
+                              console.log('onBoardBtnPausePlayClick');
+                  onBoardBtnPausePlayClick();
+
+                  }
+              } >
+            <i className="fa fa-play-circle-o fa-2x"></i>
       </div>
+
     </div>
     );
-  }
+  };
 
-});
 
 module.exports = BoardBtnControls;
