7f434105214c15739e28e3694907e5adf25648ab
sethsilesky <seth.silesky@gmail.com>
2016-05-28 00:37:27 -0500

    counts down until time is less than zero

--- app/actions/_actionCreators.js
+++ app/actions/_actionCreators.js
@@ -1,4 +1,3 @@
-import { getNextId } from '../util';
 
 const _setTickingTrue = (id) => ({ type: 'SET_TICKING_TRUE', id });
 const _setTickingFalse = (id) => ({ type: 'SET_TICKING_FALSE', id });
@@ -11,9 +10,18 @@ export const decrement = (id) => ({ type: 'DECREMENT', id });
 export const startTicking = (id) => {
     return (dispatch, getState) => {
       console.log(getState());
-      window.myInt = setInterval(() => dispatch(decrement(id)), 1000);
       dispatch(_setTickingTrue(id));
-
+      window.myInt = setInterval(() => {
+        let currentTime = getState().find((el) => el.id === id).time;
+    /* based on the current id, find the current time. as long as current time is
+        more than zero, keep counting down. */
+        if (currentTime > 0) {
+          dispatch(decrement(id));
+            } else {
+          clearInterval(window.myInt);
+          console.log('Times up. Next timer should start...');
+      } // ^ called every 1 second
+    }, 1000);
     };
   };
   export const pauseTimer = (id) => {
--- app/util.js
+++ app/util.js
@@ -4,7 +4,24 @@ export const ifZero = () => {
   if (this.props.eachTime < 1 && this.props.eachTicking) {
     console.log('ifZero: if statement passed (time should be less than 1, current timer should be ticking)');
 }
+
+
 /*
+export const getNextId = (stateArr = this.props.state.getState(), currentId = this.props.eachKey) => {
+  const _getCurrentValueFromStateArr = () => {
+    return stateArr.find((el) => el.id === currentId); 
+  };
+  const _getNextIdFromCurrentValue = (currentValue) => {
+    let index = stateArr.indexOf(currentValue);
+    if (index >= 0 && index < stateArr.length - 1) {
+      const nextItem = stateArr[index + 1];
+      return nextItem.id;
+    } else {
+      return 0;
+    }
+  }
+  return _getNextIdFromCurrentValue(_getCurrentValueFromStateArr());
+};
 
 - set ticking false
 -  clear interval
