e55b5c03d86fb0690181ad6662369a8d958c16de
Seth Silesky <seth.silesky@gmail.com>
2016-05-31 13:42:14 -0500

    indent

--- test/test_reducer.js
+++ test/test_reducer.js
@@ -11,24 +11,24 @@ module.exports = function() {
   describe('* * * reducer * * *', function() {
     describe('set ticking true:', function() {
       it('store.getState()[0].ticking should return true when action: SET_TICKING_TRUE is dispatched.', function() {
-      store.dispatch({ type: 'SET_TICKING_TRUE', id: 1 })
-      assert.equal(store.getState()[0].ticking, true);
+        store.dispatch({ type: 'SET_TICKING_TRUE', id: 1 })
+        assert.equal(store.getState()[0].ticking, true);
+      });
+    });
+    describe('set ticking false:', function() {
+      it('store.getState()[0].ticking should return false after action: SET_TICKING_FALSE is dispatched.', function() {
+        store.dispatch({ type: 'SET_TICKING_TRUE', id: 1 })
+        store.dispatch({ type: 'SET_TICKING_FALSE', id: 1 });
+        assert.equal(store.getState()[0].ticking, false);
+      });
+    });
+    describe('startTicking():', function() {
+      it('store.getState()[0].ticking should return true after action: startTicking() is dispatched.', function() {
+        store.dispatch(actionCreators.startTicking(1));
+        assert.equal(store.getState()[0].ticking, true);
+        clearInterval(window.myInt);
+      });
     });
   });
-  describe('set ticking false:', function() {
-    it('store.getState()[0].ticking should return false after action: SET_TICKING_FALSE is dispatched.', function() {
-      store.dispatch({ type: 'SET_TICKING_TRUE', id: 1 })
-      store.dispatch({ type: 'SET_TICKING_FALSE', id: 1 });
-      assert.equal(store.getState()[0].ticking, false);
-    });
-  });
-  describe('startTicking():', function() {
-    it('store.getState()[0].ticking should return true after action: startTicking() is dispatched.', function() {
-      store.dispatch(actionCreators.startTicking(1));
-      assert.equal(store.getState()[0].ticking, true);
-      clearInterval(window.myInt);
-    });
-  });
-});
 }
 //I think i need to pass in the clear interval component
